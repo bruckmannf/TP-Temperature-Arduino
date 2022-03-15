9771699e12c709dab660b2a6ae75b1dd8816e1d9
pujol <pujol.galdric@gmail.com>
2022-03-15 12:47:36 +0100

    hello world

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,23 @@
-#include <Arduino.h>
+#include <M5CoreInk.h>
 
+Ink_Sprite InkPageSprite(&M5.M5Ink);
+
+//Initialisation
 void setup() {
-  // put your setup code here, to run once:
+    M5.begin();
+    M5.M5Ink.clear();
+    if( !M5.M5Ink.isInit()){
+        Serial.printf("Ink Init faild");
+        while (1) delay(100);
+    }
+    if( InkPageSprite.creatSprite(0,0,200,200,true) != 0 ){
+        Serial.printf("Ink Sprite creat faild");
+    }
+    //digitalWrite(LED_EXT_PIN,LOW);
+    InkPageSprite.drawString(35,50,"Hello World!"); //draw string. 绘制字符串
+    InkPageSprite.pushSprite();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    int a=1;
 }
